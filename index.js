f47fbe264cd4cd0fc89a6966093a07a2e2f7f677
Morgas <morgas01@web.de>
2016-07-26 20:43:29 +0200

    make usable in NIWA

---
+++ index.js
@@ -0,0 +1,15 @@
+var path=require("path");
+var less=require("less");
+exports.dirname=path.resolve(__dirname,"src");
+exports.getStyle=function(component,theme)
+{
+	return less.render(
+		'@import "'+path.resolve(exports.dirname,"less","theme","default.less")+'";'+
+		'\n@import "'+path.resolve(exports.dirname,"less","style",component)+'";'+
+		'\n@import "'+path.resolve(exports.dirname,"less","structure",component)+'";'+
+		(theme? '\nimport "'+path.resolve(exports.dirname,"less","theme",theme+".less")+'";':"")
+	).then(
+		data=>data.css,
+		error=>Promise.reject(JSON.stringify(error))
+	);
+}
