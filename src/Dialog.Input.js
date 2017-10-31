11daa37855a07e8af79bbe075588b25ee43b2787
Morgas <morgas01@web.de>
2017-10-31 05:50:45 +0100

    comment

--- src/Dialog.Input.js
+++ src/Dialog.Input.js
@@ -43,7 +43,8 @@
 					{
 						this._signal.resolve();
 					},
-					µ.constantFunctions.n);//hide uncaught error);
+					//@suppress UnhandledPromiseRejectionWarning
+					µ.constantFunctions.n);
 				}
 			};
 			param.dialogTagName="FIELDSET";
