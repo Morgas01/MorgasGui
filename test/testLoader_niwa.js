a8dbbd259a82562d9899f8c5733e68c1798bc010
Maximilian Kükelheim <maximilian.kuekelheim@it-motive.de>
2018-03-02 14:26:59 +0100

    use niwa special context

--- test/testLoader_niwa.js
+++ test/testLoader_niwa.js
@@ -3,7 +3,7 @@
 	window.tests={
 		loadMorgas:function(name)
 		{
-			document.write(String.raw`<script type="application/javascript" charset="utf-8" src="/Morgas.js/src/${name}.js" defer></script>`);
+			document.write(String.raw`<script type="application/javascript" charset="utf-8" src="/morgas/${name}.js" defer></script>`);
 		},
 		loadTest:function(name,noStyle)
 		{
